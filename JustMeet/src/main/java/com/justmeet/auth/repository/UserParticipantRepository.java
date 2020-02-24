a929540bac04d9b3660a2ee8120d7b8ced9fb49b
Vittorio Rinaldi <vittorio.rinaldi@studenti.unicam.it>
2020-02-24 17:45:51 +0100

    aggiust. pacchetti

--- JustMeet/src/main/java/com/justmeet/auth/repository/UserParticipantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.justmeet.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.justmeet.auth.model.User;
-
-public interface UserParticipantRepository extends JpaRepository<User, Long> {
-
-}
